c876f654fdad1c254ec07c53f8bf9f10ca42f8eb
hightt <konrad.duda121@gmail.com>
2025-05-06 17:42:41 +0200

    Basic landing page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -14,6 +14,12 @@ class HomeController extends AbstractController
 {
     static int $maxRecentJoinDays = 30;
 
+    #[Route('/landing-page', name: 'app_landing_page')]
+    public function landingPage(
+    ): Response {
+        return $this->render('landing_page.html.twig', []);
+    }
+
     #[Route('/', name: 'app_homecontroller')]
     public function index(
         EmployeeRepository   $employeeRepository,
