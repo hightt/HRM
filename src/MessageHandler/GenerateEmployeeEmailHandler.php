301321c615927a0f51ca1d8bf37e14190527f523
hightt <konrad.duda121@gmail.com>
2025-06-12 20:48:55 +0200

    code review

--- src/MessageHandler/GenerateEmployeeEmailHandler.php
+++ src/MessageHandler/GenerateEmployeeEmailHandler.php
@@ -3,15 +3,9 @@ declare(strict_types=1);
 
 namespace App\MessageHandler;
 
-use Twig\Environment;
 use Psr\Log\LoggerInterface;
-use App\Repository\WorkLogRepository;
-use Symfony\Component\Mailer\MailerInterface;
 use App\Model\Message\GenerateEmployeeReportMessage;
-use App\Service\Employee\EmployeeDocumentGeneratorService;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
-use Symfony\Component\DependencyInjection\Attribute\TaggedLocator;
-use Symfony\Component\DependencyInjection\Attribute\TaggedIterator;
 
 #[AsMessageHandler]
 class GenerateEmployeeEmailHandler
@@ -20,10 +14,6 @@ class GenerateEmployeeEmailHandler
     * @param EmployeeEmailHandlerInterface[] $handlers
     */
     public function __construct(
-        private MailerInterface                  $mailer,
-        private WorkLogRepository                $workLogRepository,
-        private EmployeeDocumentGeneratorService $employeeDocumentGeneratorService,
-        private Environment                      $twig, 
         private LoggerInterface                  $logger,
         private iterable                         $handlers,
     ) {}
